2735883e818d055e55007e7b779c70bd4634c631
Gururaj V <gururaj.venkataramana@honeywell.com>
2020-01-16 10:23:57 +0530

    Initial commit

---
+++ app/src/main/java/com/dgw/logintest/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.dgw.logintest.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult (
+     val success:LoggedInUserView? = null,
+     val error:Int? = null
+)
